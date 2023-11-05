b89432d8e318bdc628c4c90ac49171c6d6b0fa30
Noah <noahsternerr@gmail.com>
2023-11-05 12:53:04 +0100

    hero scroller tillagd

---
+++ src/components/Scroller.vue
@@ -0,0 +1,114 @@
+<template>
+    <div class="scroller">
+        <div class="scroller__wrapper flex">
+            <img src="~/assets/img/landingheader.png" alt="">
+            <img src="~/assets/img/landingheader.png" alt="">
+            <img src="~/assets/img/landingheader.png" alt="">
+        </div>
+        <div class="scroller__navigation-buttons flex">
+            <div role="button" data-isVisible="true"></div>
+            <div role="button" data-isVisible="false"></div>
+            <div role="button" data-isVisible="false"></div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    const currentIndex = ref(0);
+
+    const setSlidePosition = (index: number): number => {
+        const items = document.querySelectorAll(".scroller__wrapper img");
+        const slideWidth = items[0].getBoundingClientRect().width;
+
+        return (slideWidth * index);
+    }
+
+    onMounted(() => {
+        const buttons = document.querySelectorAll(".scroller__navigation-buttons div");
+        const track = document.querySelector(".scroller__wrapper");
+
+        buttons.forEach((button, index) => {
+            button.addEventListener("click", (e) => {
+                const slideWidth: number = setSlidePosition(index);
+                
+                track?.scrollTo({
+                    left: slideWidth,
+                    behavior: "smooth"
+                });
+
+                buttons[currentIndex.value].setAttribute("data-isVisible", "false");
+                button.setAttribute("data-isVisible", "true");
+
+                currentIndex.value = index;
+            }) 
+        })
+    })
+</script>
+
+<style>
+    .scroller {
+        width: 100%;
+        height: 350px;
+    }
+
+    .scroller__wrapper {
+        gap: 0;
+        width: 100%;
+        min-width: 100%;
+        min-height: 35px;
+
+        overflow-x: scroll;
+
+        scroll-snap-type: inline mandatory;
+        scrollbar-width: none;
+    }
+    
+    .scroller__wrapper > * {
+        scroll-snap-align: start;
+    }
+
+    .scroller__wrapper img {
+        min-width: 100%;
+        height: 350px;
+
+        border-radius: 15px;
+
+        object-fit: cover;
+        /* object-position: 25% 25%;  */
+    }
+
+    .scroller .scroller__navigation-buttons {
+        --gap: 8px;
+        margin-top: 16px;
+    }
+    
+    .scroller .scroller__navigation-buttons div {
+        height: 14px;
+        width: 100px;
+
+        border-radius: 8px;
+        background-color: hsl(var(--clr-surface-on));
+
+        cursor: pointer;
+    }
+
+    .scroller .scroller__navigation-buttons div[data-isVisible="false"] {
+        opacity: 0.5;
+    }
+
+
+    @media (max-width: 46rem) {
+        .scroller__wrapper {
+            gap: 16px;
+            scrollbar-width: thin;
+        } 
+
+        .scroller__wrapper img {
+            min-width: 90%;
+        }
+
+        .scroller .scroller__navigation-buttons {
+            display: none;
+        }
+    }
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="landing">
+        <div class="hero container">
+            <div class="hero-wrapper grid">
+                <div class="hero__info">
+                    <h1>Ge din bil ett nytt leende</h1>
+                    <p>
+                        Här på recond återställer vi inte bara utseendet; vi skapar en upplevelse. 
+                        Vi ger ditt fordon den perfekta vården. 
+                        Boka din tid redan idag och upplev känslan av ett fordon i perfektion.
+                    </p>
+                    <div class="hero__info__button flex">
+                        <div class="line"></div>
+                        <button class="primary-button-outlined">Boka Här</button>
+                    </div>
+                </div>
+                <Scroller />
+            </div>  
+        </div>
+    </div>
+</template>
+
+
+<style>
+    .hero {
+        padding: 0 32px;
+    }
+
+    .hero-wrapper {
+
+        --gap: 125px;
+
+        grid-template-columns: 0.9fr 1fr;
+
+        max-width: 1280px;
+        margin: 180px auto;
+
+        align-items: center;
+    }
+
+    .hero-wrapper .hero__info p {
+        margin-top: 16px;
+    }
+
+    .hero-wrapper .hero__info .hero__info__button {
+        --gap: 16px;
+        margin-top: 32px;
+
+        justify-content: start;
+        align-items: center;
+    }
+
+    .hero-wrapper .hero__info .line {
+        width: 44px;
+        height: 1px;
+
+        background-color: hsl(var(--clr-primary));
+    }
+
+    @media (max-width: 62.5rem) {
+        .hero-wrapper {
+            --gap: 32px;
+        }
+    }
+
+    @media (max-width: 46rem) {
+        .hero-wrapper {
+            --gap: 64px;
+            grid-template-columns: 1fr;
+        }
+    }
+</style>
